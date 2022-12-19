4fa285074bc324923861887c2045d78328328485
Dmitriy <fpmt2010@rambler.ru>
2022-12-19 23:09:28 +0300

    addCatalog

---
+++ Dz6FinalProject/Catalog.java
@@ -0,0 +1,9 @@
+package Dz6FinalProject;
+
+public class Catalog extends Laptop {
+
+    public Catalog(int id, int ram, int ssd, String os, String color){
+        super(id,ram,ssd,os,color);
+    }
+ 
+}
